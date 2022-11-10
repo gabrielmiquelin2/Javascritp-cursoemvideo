14d95a78844eb6e504462732ebfa8442a1f33bcb
gabrielferreiramiquelin@gmail.com <gabrielferreiramiquelin@gmail.com>
2022-11-10 17:45:56 -0300

    condicionais

---
+++ Aulas/video11/ex008.js
@@ -0,0 +1,13 @@
+//CONDIÇÂO SIMPLES
+
+let velocidade = 55
+
+console.log (`A velocidade do seu carro é ${velocidade}km/h`)
+
+if(velocidade > 60 ){
+    console.log("Você ultrapassou o limite de velocidade permitido")
+}else{
+    console.log("Segue o baile")
+}
+
+console.log(`Diriga sempre usando cinto de segurança`)
---
+++ Aulas/video11/ex009.js
@@ -0,0 +1,9 @@
+//CONDIÇÂO COMPOSTA
+let pais = "Brasil"
+console.log(`Vivendo em ${pais}`)
+
+if (pais != "Brasil"){// != -> diferente
+  console.log("Você é estrangeiro")
+}else{
+   console.log("Você é brasileiro")
+}
