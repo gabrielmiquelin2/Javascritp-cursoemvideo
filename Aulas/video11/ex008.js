1d35c431f9d1f598a19f9ac6777aa666dd953edc
gabrielferreiramiquelin@gmail.com <gabrielferreiramiquelin@gmail.com>
2023-02-05 18:24:29 -0300

    js

--- Aulas/video11/ex008.js
+++ Aulas/video11/ex008.js
@@ -7,7 +7,7 @@ console.log (`A velocidade do seu carro é ${velocidade}km/h`)
 if(velocidade > 60 ){
     console.log("Você ultrapassou o limite de velocidade permitido")
 }else{
-    console.log("Segue o baile")
+    console.log("Segue o baile menor")
 }
 
 console.log(`Diriga sempre usando cinto de segurança`)
