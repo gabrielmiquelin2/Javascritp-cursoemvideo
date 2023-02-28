6dc5c72858dfe7114db1eed9436220fac693972b
Gabriel Ferreira Miquelin <gabrielferreiramiquelin@gmail.com>
2023-02-27 22:00:08 -0300

    vetores

--- Aulas/video15final/js.js
+++ Aulas/video15final/js.js
@@ -1,3 +1,6 @@
 let num = [3,2,3,7,8]
-
-console.log(`Nosso vetor é ${num}`)
+num.push(1) //acrescentando mais um numero no vetor
+num.sort() //colocar em ordem crescente
+console.log(num)
+console.log(`O vetor tém ${num.length} posição`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
